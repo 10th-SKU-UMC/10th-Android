fcf5bb3c123ee3acbeec13b628834cfe39cda828
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-05-20 02:55:53 +0900

    feat: Navigation 순서 수정

--- week08/app/src/main/java/com/example/and_practice/presentation/navigation/BottomNavItem.kt
+++ week08/app/src/main/java/com/example/and_practice/presentation/navigation/BottomNavItem.kt
@@ -2,8 +2,8 @@ package com.example.and_practice.presentation.navigation
 
 enum class BottomNavItem(val label: String) {
     HOME("홈"),
+    PURCHASE("구매하기"),
     WISH("위시리스트"),
     CART("장바구니"),
-    PURCHASE("구매하기"),
     MY_PAGE("프로필")
 }
