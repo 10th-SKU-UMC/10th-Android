bbd2839f00bcb6e17bb7d1e3a136bce2e2250e9e
ljw42b <80021912+JiwonLee42@users.noreply.github.com>
2026-05-20 02:41:48 +0900

    feat: 네비게이션 및 메인 화면 구성

---
+++ week08/app/src/main/java/com/example/and_practice/presentation/navigation/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.example.and_practice.presentation.navigation
+
+enum class BottomNavItem(val label: String) {
+    HOME("홈"),
+    WISH("위시리스트"),
+    CART("장바구니"),
+    PURCHASE("구매하기"),
+    MY_PAGE("프로필")
+}
